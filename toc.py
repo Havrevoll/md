049edf528e0a0abe2d18086fe9f0541a4e6ba606
Ola Haugen Havrevoll <havrevoll@gmail.com>
2022-11-29 23:06:13 +0100

    Fixed "next free track"

--- toc.py
+++ toc.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import bitstring
 import argparse
+import shutil
 
 
 # bitstring.BitArray("0b"+f"{0x40:014b}"+f"{0x19:06b}"+f"{0x6:04b}").bytes
@@ -26,6 +27,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("cuefile", nargs='?')
     parser.add_argument("binfile", nargs='?')
+    # parser.add_argument("outfile", nargs='?')
     args = parser.parse_args()
 
     seconds = read_cuefile(args.cuefile)
@@ -36,17 +38,17 @@ if __name__ == '__main__':
 
     start_enc = [bitstring.BitArray("0b"+f"{a[0]:014b}"+f"{a[1]:06b}"+f"{a[2]:04b}").bytes for a in start]
     end_enc = [bitstring.BitArray("0b"+f"{a[0]:014b}"+f"{a[1]:06b}"+f"{a[2]:04b}").bytes for a in end]
-    # enc = 
 
-
+    for i in range(10):
+        backupfile = Path(args.binfile+".bak"+str(i)).absolute()
+        if not backupfile.exists():
+            shutil.copy2(Path(args.binfile).absolute(), backupfile)
+            break
+        
     with open(args.binfile, 'r+b')  as f:
-        f.seek(0x92f)
-        lastref = int.from_bytes(f.read(1),byteorder='big')
-
-
         f.seek(0x13c)
-        sistespor  = f.read(3)
-        end_enc[-1] = sistespor
+        last_track  = f.read(3)
+        end_enc[-1] = last_track
         f.seek(0x1f)
         f.write(len(end_enc).to_bytes(1,byteorder='big'))
         f.seek(0x30)
@@ -57,13 +59,20 @@ if __name__ == '__main__':
         for a,b in zip(start_enc,end_enc):
             l = l + a + bytes.fromhex('a6')+b +  bytes.fromhex('00')
             track = track + bytes.fromhex('00')*6 + bytes.fromhex('01')+ bytes.fromhex('20')
-
+        
+        f.seek(0x92f)
+        lastref = int.from_bytes(f.read(1),byteorder='big')
         if lastref != 0:
             f.seek(lastref*8 + 0x130)
             last_fragment = f.read(7)
             l = l + last_fragment
             f.seek(0x92f)
             f.write((len(end_enc) + 1).to_bytes(1,byteorder='big'))
+        
+        f.seek(0x2f)
+        if not int.from_bytes(f.read(1),byteorder='big') == 255:
+            f.seek(0x2f)
+            f.write((len(end_enc)+1).to_bytes(1,byteorder='big'))
         f.seek(0x138)
         f.write(l)
         f.tell()
