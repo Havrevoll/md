d1a51385735705a6f5fb306badb1e57674f6cf3e
Ola Haugen Havrevoll <havrevoll@gmail.com>
2023-05-16 08:59:23 +0200

    Oppdatert cue_parse

--- cue_parse.py
+++ cue_parse.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from pyleri import (
     Grammar,
     Regex,
@@ -47,17 +48,19 @@ class CueSheetGrammar(Grammar):
         )
     )
 
-    track = Sequence(
+    tracks = Sequence(
         track_header,
         Repeat(track_meta),
         Repeat(index),
     )
     
+    disc_header = Repeat(track_meta)
+
     # Define rules for each part of the cue sheet file.
     START = Sequence(
-        Repeat(track_meta),
+        disc_header,
         file_header,
-        Repeat(track),
+        Repeat(tracks),
     )
 
 def node_props(node, children):
@@ -77,17 +80,43 @@ def view_parse_tree(res):
     start = res.tree.children[0] if res.tree.children else res.tree
     return node_props(start, get_children(start.children))
 
-if __name__ == "__main__":
-    cue_parser = CueSheetGrammar()
-    # with open("profound.cue") as in_file:
-    #     text = in_file.read()
+def find_elem(tree, name):
+    for i, a in enumerate(tree['children']):
+        if len(a['children']) > 0 and a['children'][0]['string'] == name:
+            return a['children'][1]['string']
+        elif len(a['children']) > 0:
+            return find_elem(a, name)
+    return None
 
-    with open("profound.cue", "r", encoding='utf-8') as f:
+cue_parser = CueSheetGrammar()
+            
+def cue_parse(file):
+    with open(file, "r", encoding='utf-8') as f:
         # read lines and filter out those that start with "REM"
         lines = [line.strip() for line in f if not line.strip().startswith("REM")]
     
         # join remaining lines into a single string
         text = "\n".join(lines)
     result = cue_parser.parse(text)
-    print(result.is_valid)
-    print(view_parse_tree(result))
+    assert result.is_valid
+    tre = view_parse_tree(result)
+ 
+    res_disc_title = find_elem(tre['children'][0], "TITLE")
+    res_disc_performer = find_elem(tre['children'][0], "PERFORMER")
+    res_track_number = len(tre['children'][2])
+    res_tracks = []
+    for i in range(res_track_number):
+        track_entry = tre['children'][2]['children'][i]['children']
+        track_dict = {}
+        track_dict['title'] = find_elem(track_entry[1], "TITLE")
+        track_dict['performer'] = find_elem(track_entry[1], "PERFORMER")
+        track_dict['number'] = int(track_entry[0]['children'][1]['string'])
+        assert int(track_entry[2]['children'][-1]['children'][1]['string']) == 1
+        track_dict['index'] = track_entry[2]['children'][-1]['children'][2]['string']
+        res_tracks.append(track_dict)        
+    return {'title':res_disc_title,'performer':res_disc_performer, 'tracks':res_tracks}
+
+
+if __name__ == "__main__":
+    disc = cue_parse("profound.cue")
+    print(disc)
